b9060cc5038052481be369f5e0a67ea9ebbdf435
Vladyslav Kolomiichuk <vlad1710@ukr.net>
2024-02-13 15:30:05 +0200

    2

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -5,15 +5,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -24,21 +27,34 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
 
-    val colorScheme = lightColorScheme(primary = Color.Red)
     var visible by remember { mutableStateOf(false) }
 
-    MaterialTheme(colorScheme = colorScheme) {
+    MaterialTheme {
         Column(
             Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Content()
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                )
+            )
             Spacer(modifier = Modifier.height(32.dp))
             Button(
                 onClick = { visible = true }
@@ -47,25 +63,26 @@ fun App() {
             }
 
             if (visible)
-                ModalBottomSheet(
+                Dialog(
+                    onDismissRequest = { visible = false  }
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(150.dp)
+                            .background(MaterialTheme.colorScheme.primary)
+                    )
+                }
+               /* ModalBottomSheet(
                     onDismissRequest = { visible = false }
                 ) {
-                    Content()
-                }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(150.dp)
+                            .background(MaterialTheme.colorScheme.primary)
+                    )
+                }*/
         }
-    }
-}
-
-@Composable
-fun Content() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Primary color")
-        Spacer(modifier = Modifier.height(32.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .background(MaterialTheme.colorScheme.primary)
-        )
     }
 }
